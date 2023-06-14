c358b6635a0f299bc72af2a78452dd233a10eb31
GODofGOD007 <112172428+GODofGOD007@users.noreply.github.com>
2023-06-14 21:32:49 +0530

    Add files via upload

---
+++ leap_year.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+
+   int year;
+   printf("enter a year : \n");
+   scanf("%d", &year);
+
+   if (year % 4 == 0)
+      printf("%d is a leap year", year);
+
+   else
+      printf("%d is not a leap year", year);
+}
