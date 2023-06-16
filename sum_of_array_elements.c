af708f108154850fb9491c0d3aec41942b32c68d
GODofGOD007 <112172428+GODofGOD007@users.noreply.github.com>
2023-06-16 21:09:25 +0530

    Add files via upload

---
+++ sum_of_array_elements.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], size, i, sum = 0;
+
+    printf("Enter array size\n");
+    scanf("%d", &size);
+
+    printf("Enter array elements\n");
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    printf("Sum of the array = %d\n", sum);
+
+    return 0;
+}
